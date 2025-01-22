9fdcabbb6d9e50984bdcc39b2f82c395a5e34dce
ParthPanchal26 <parthpanchal2625@gmail.com>
2025-01-22 17:39:52 +0530

    Completed implementation of home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -17,6 +17,19 @@ const Home = () => {
 
       <img className='hero_cropcare_pic' src="cropcare_hero.jpg" alt="" />
 
+      <section className="section_2">
+
+        <div className="part_1">
+          <div className='title'>Empowering Farmers with AI: Discover the Future of Farming Assistance</div>
+          <div className='part_1_desc'>Our AI assistant chatbot revolutionizes farming practices by providing real-time support and accurate plant disease identification.</div>
+          <li className='bullets'>Real-time plant disease identification for healthier crops</li>
+          <li className='bullets'>Optimize your farming practices with AI-powered insights</li>
+          <li className='bullets'>Expert farming advice at your fingertips</li>
+        </div>
+
+        <img className='plant_pic' src="plant.jpg" alt="plant.jpg" />
+      </section>
+
     </div>
   )
 }
